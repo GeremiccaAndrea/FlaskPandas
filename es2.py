3eec10c0e091e6c2dc38a338b9acdc3656c2b089
Andrea Geremicca <geremicca.andrea@iisgalvanimi.edu.it>
2023-04-24 13:16:30 +0000

    commit

--- es2.py
+++ es2.py
@@ -5,10 +5,9 @@ import pandas as pd
 dati_film = pd.read_csv('https://raw.githubusercontent.com/wtitze/3E/main/2010.csv', sep = ';')
 
 @app.route('/')
-def home():
-    film_inglesi = dati_film[dati_film['Language'] == 'English']
-    tablefilm_inglesi = film_inglesi.to_html() #trasformo in html
-    return render_template('es1.html', tabella = tablefilm_inglesi )
+def search():
+  lingua = request.args('lingua')
+  return render_template('es2.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
